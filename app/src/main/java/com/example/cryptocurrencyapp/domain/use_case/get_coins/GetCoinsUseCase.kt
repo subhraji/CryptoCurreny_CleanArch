5e7a8b43afa7888bd0f04f7e1a623478ce001f9f
subhrajitdeb <subhrajitdeb54@gmail.com>
2023-08-28 00:24:50 +0530

    first commit

---
+++ app/src/main/java/com/example/cryptocurrencyapp/domain/use_case/get_coins/GetCoinsUseCase.kt
@@ -0,0 +1,28 @@
+package com.example.cryptocurrencyapp.domain.use_case.get_coins
+
+import com.example.cryptocurrencyapp.common.Resource
+import com.example.cryptocurrencyapp.data.remote.dto.CoinDto
+import com.example.cryptocurrencyapp.data.remote.dto.toCoin
+import com.example.cryptocurrencyapp.domain.model.Coin
+import com.example.cryptocurrencyapp.domain.repository.CoinRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
+import javax.inject.Inject
+
+class GetCoinsUseCase @Inject constructor(
+    private val repository: CoinRepository
+) {
+    operator fun invoke(): Flow<Resource<List<Coin>>> = flow {
+        try {
+            emit(Resource.Loading())
+            val coins = repository.getCoins().map { it.toCoin() }
+            emit(Resource.Success(coins))
+        } catch (e: HttpException){
+            emit(Resource.Error( e.localizedMessage ?: "An unexpected error occured"))
+        } catch (e: IOException){
+            emit(Resource.Error( "Could not reach server."))
+        }
+    }
+}
